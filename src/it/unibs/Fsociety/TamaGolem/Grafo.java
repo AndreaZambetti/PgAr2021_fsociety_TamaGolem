4d7b0d6b8b839ac05c1a0731db53d8ea917b249e
d-martinelli1612 <d.martinelli1612@gmail.com>
2021-05-08 20:16:04 +0200

    Salvataggio progressi
    Programma non operativo e non testato

--- src/it/unibs/Fsociety/TamaGolem/Grafo.java
+++ src/it/unibs/Fsociety/TamaGolem/Grafo.java
@@ -1,11 +1,10 @@
 package it.unibs.Fsociety.TamaGolem;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 public class Grafo {
 
-    private static int range = 10;
+    private static int range = 11;
     private static int minimo_danni = 10;
 
     private int dimensione;
@@ -33,19 +32,27 @@ public class Grafo {
 
     public void imposta_interazione_elementi(){
 
-        int quanti_subiscono, quanti_infieriscono;
+        int quanti_subiscono, quanti_infieriscono, somma, effetto, casi_da_assegnare = 0;
+        Set<Integer> elenco_elementi_assegnati = new HashSet<>();
 
         for (int i=0; i<this.dimensione; i++){
-            for (int j=0; j<this.dimensione; j++){
-                /*Grazie all'impostazione del costruttore il valore 1 è considerato indicatore
-                  di effetto non ancora impostato*/
-                if (matrice_adiacenze[i][j] == 1){
 
-                    //selezione di quanti elementi subiranno danno dall'elemento in questione
-                    quanti_subiscono = (int) (Math.random() * (dimensione - 2) + 1);
+            //Controlla quanti elementi in una riga devono essere ancora assegnati
+            for (int j=0; j<dimensione; j++)
+                if (this.matrice_adiacenze[i][j] == 1)
+                    casi_da_assegnare++;
 
+            //Stabilisce quanti elementi provocano o ricevono effetti all'elemento in analisi
+            quanti_subiscono = (int) (Math.random() * (casi_da_assegnare - 2) + 1);
+            quanti_infieriscono = casi_da_assegnare - 1 - quanti_subiscono;
+
+            //Stabilisce quali elementi subiscono danni dall'elemento in analisi
+            for (int j=0; j<quanti_subiscono; j++)
+                /*continua a selezionare casualmente finché non ottiene gli indici richiesti,
+                * inoltre nessuno degli indici deve essere uguale a quello dell'elemento in analisi*/
+                while (elenco_elementi_assegnati.size() < quanti_subiscono && elenco_elementi_assegnati.contains(i)) {
+                    elenco_elementi_assegnati.add((int) (Math.random() * dimensione));
                 }
-            }
         }
     }
 }
